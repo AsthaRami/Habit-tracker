d61353e7505cbe3fe5e51b36ec80cba869518e9a
Astha Rami <ramiaastha80@gmail.com>
2026-03-11 22:01:40 +0530

    all changes done

--- habits/urls.py
+++ habits/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.index_view, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
--- habits/views.py
+++ habits/views.py
@@ -17,6 +17,11 @@ def register_view(request):
         form = UserCreationForm()
     return render(request, 'habits/register.html', {'form': form})
 
+def index_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    return render(request, 'habits/index.html')
+
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
@@ -30,7 +35,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect('index')
 
 @login_required
 def dashboard(request):
